978c5991e92fd5e42a134d92c171e78de15d3ded
Italo Rhaffael <italorhafael@hotmail.com>
2024-06-21 00:37:24 -0300

    minhas mudanças
    criei dois arquivos ts para externar os dados dos lanches mas ainda não terminei o serviço. irei deixar tudo pronto para receber as requisições da api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,15 +16,12 @@ const routes: Routes = [
     path: 'tela-cadastro',
     loadChildren: () => import('./tela-cadastro/tela-cadastro.module').then( m => m.TelaCadastroPageModule)
   },
-  {
-    path: 'cadastro-vendedor',
-    loadChildren: () => import('./cadastro-vendedor/cadastro-vendedor.module').then( m => m.CadastroVendedorPageModule)
-  }
+  
 ];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
-})
+})  
 export class AppRoutingModule {}
---
+++ src/app/tab3/lanche.ts
@@ -0,0 +1,9 @@
+//esse é o typescript que exporta as informações
+export interface Lanche {
+    id: number;
+    nome: string;
+    descricao: string;
+    preco: number;
+    imagemUrl: string;
+  }
+  
